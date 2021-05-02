1cea681dc66f281a9549a4593b14a7770d92f9d4
Rdoy <hridoykarmakar883@gmail.com>
2021-05-02 19:17:21 +0630

    added

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css';
+import Typical from 'react-typical'
 
 const Header = () => {
     return (
@@ -8,7 +9,17 @@ const Header = () => {
             <div>
                 <h5 className="text-center pt-5" style={{color: '#3e64ff'}}>HEY! I AM</h5>
                 <h1 className="text-center">Hridoy karmakar</h1>
-                <h2 className="text-center" >I'm a <span style={{color: '#3e64ff'}}>Developer</span></h2>
+                <h2 className="text-center" >
+                    I'm a 
+                <span style={{color: '#3e64ff'}}>
+                <Typical
+                    
+                    loop={Infinity}
+                    wrapper="b"
+                    steps={[' Web Developer', 3000, ' Programmer', 3000, ' Web Designer', 3000]}
+                />
+                </span>
+                </h2>
                 
             </div>
         </section>
