07d9525b8303b11902ec8bb7a0619230b30a65a3
Rdoy <hridoykarmakar883@gmail.com>
2021-05-03 21:22:32 +0630

    added

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import About from '../About/About';
-import Experience from '../Experience/Experience';
 import Footer from '../Share/Footer/Footer';
 import Header from '../Header/Header';
 import Message from '../Message/Message';
